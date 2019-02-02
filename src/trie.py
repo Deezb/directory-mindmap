f2c6d880c3204cb52f084bc1f5a52decbef16fe3
David Bryan <deezbryan@gmail.com>
2019-02-02 13:32:41 +0000

    renamed variables to better explain their purpose, commented some of the functions. Next stage will be to add direct links for opening folders or files

--- src/trie.py
+++ src/trie.py
@@ -4,8 +4,8 @@ import os
 
 
 class TrieNode(object):
-    def __init__(self, title: str):
-        self.title = title
+    def __init__(self, dirOrFile: str):
+        self.dirOrFile = dirOrFile
         self.children = []
         self.word_finished = False
         self.counter = 1
@@ -13,11 +13,11 @@ class TrieNode(object):
 
 def add(root, word: str):
     node = root
-    for char in word.split("\\"):
+    for dirOrFile in word.split("\\"):
         found_in_child = False
         # Search for the character in the children of the present `node`
         for child in node.children:
-            if child.char == char:
+            if child.dirOrFile == dirOrFile:
                 # We found it, increase the counter by 1 to keep track that another
                 # word has it as well
                 child.counter += 1
@@ -27,7 +27,7 @@ def add(root, word: str):
                 break
         # We did not find it so add a new chlid
         if not found_in_child:
-            new_node = TrieNode(char)
+            new_node = TrieNode(dirOrFile)
             node.children.append(new_node)
             # And then point node to the new child
             node = new_node
@@ -37,45 +37,59 @@ def add(root, word: str):
 
 def find_prefix(root, prefix: str) -> Tuple[bool, int]:
     """
-    Check and return
-      1. If the prefix exsists in any of the words we added so far
-      2. If yes then how may words actually have the prefix
+    Check and return tuple (1, 2)
+      1. Boolean, does the current path already exist from previous files added
+      2. If True, then how many files have been down this path
     """
     node = root
-    # If the root node has no children, then return False.
-    # Because it means we are trying to search in an empty trie
+    # no
     if not root.children:
         return False, 0
-    for char in prefix:
-        char_not_found = True
+    for dirOrFile in prefix:
+        dir_or_file_not_found = True
         # Search through all the children of the present `node`
         for child in node.children:
-            if child.char == char:
-                # We found the char existing in the child.
-                char_not_found = False
+            if child.dirOrFile == dirOrFile:
+                # We found the directory or file existing in the child.
+                dir_or_file_not_found = False
                 # Assign node as the child containing the char and break
                 node = child
                 break
         # Return False anyway when we did not find a char.
-        if char_not_found:
+        if dir_or_file_not_found:
             return False, 0
-    # Well, we are here means we have found the prefix. Return true to indicate that
-    # And also the counter of the last node. This indicates how many words have this
-    # prefix
+
     return True, node.counter
 
 
 def print_children(WholeText, WholeTextEnd, root):
+    """
+    Recursive function that adds to a string XML representation of the directory structure.
+    :param WholeText: start of xml built from parent objects, being built recursively and depth first sequence traversal
+    :param WholeTextEnd: End of xml, to be combined with WholeText when recursion is finished.
+    :param root: Current node in the directory, each folder is a node, each file is a leaf.
+    :return: return the modified string representation to include the part for this visit to the
+
+    When a directory is encountered it adds
+        <node POSITION="right" TEXT="<Dir_Name>">\n
+            calls on each child print_children(root.child)
+        </node>
+
+    when a file is encountered it adds
+        <node POSITION="right" TEXT="<File_Name>"/>\n
+    """
     endText = ""
-    newText = """<node POSITION="right" TEXT="{0}" """.format((root.char.replace("&", "...")))
+    newText = """<node POSITION="right" TEXT="{0}" """.format((root.dirOrFile.replace("&", "...")))
 
     if root.children:
+        # if the root is a directory
         WholeText = WholeText + newText
         WholeText += ">\n"
         for i in root.children:
             WholeText, WholeTextEnd = print_children(WholeText, WholeTextEnd, i)
         WholeText += "</node>\n"
     else:
+        # if the root is a file
         WholeText = WholeText + newText
         WholeText += "/>\n"
 
