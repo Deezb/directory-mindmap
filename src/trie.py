56312ac62cadb1e5ef239fe11ad94cbeee21e4b9
David Bryan <deezbryan@gmail.com>
2019-02-03 01:28:40 +0000

    Adds Links to each map leaf node to open files directly from mindmap.

--- src/trie.py
+++ src/trie.py
@@ -1,10 +1,10 @@
-from typing import Tuple
 import easygui
+from pathlib import Path
 import os
-
 
 class TrieNode(object):
     def __init__(self, dirOrFile: str):
+        self.baseDir = dirOrFile
         self.dirOrFile = dirOrFile
         self.children = []
         self.word_finished = False
@@ -13,7 +13,7 @@ class TrieNode(object):
 
 def add(root, word: str):
     node = root
-    for dirOrFile in word.split("\\"):
+    for dirOrFile in word._parts:
         found_in_child = False
         # Search for the character in the children of the present `node`
         for child in node.children:
@@ -35,34 +35,7 @@ def add(root, word: str):
     node.word_finished = True
 
 
-def find_prefix(root, prefix: str) -> Tuple[bool, int]:
-    """
-    Check and return tuple (1, 2)
-      1. Boolean, does the current path already exist from previous files added
-      2. If True, then how many files have been down this path
-    """
-    node = root
-    # no
-    if not root.children:
-        return False, 0
-    for dirOrFile in prefix:
-        dir_or_file_not_found = True
-        # Search through all the children of the present `node`
-        for child in node.children:
-            if child.dirOrFile == dirOrFile:
-                # We found the directory or file existing in the child.
-                dir_or_file_not_found = False
-                # Assign node as the child containing the char and break
-                node = child
-                break
-        # Return False anyway when we did not find a char.
-        if dir_or_file_not_found:
-            return False, 0
-
-    return True, node.counter
-
-
-def print_children(WholeText, WholeTextEnd, root):
+def print_children(WholeText, WholeTextEnd, root, path):
     """
     Recursive function that adds to a string XML representation of the directory structure.
     :param WholeText: start of xml built from parent objects, being built recursively and depth first sequence traversal
@@ -79,19 +52,20 @@ def print_children(WholeText, WholeTextEnd, root):
         <node POSITION="right" TEXT="<File_Name>"/>\n
     """
     endText = ""
-    newText = """<node POSITION="right" TEXT="{0}" """.format((root.dirOrFile.replace("&", "...")))
+    newText = """<node TEXT="{0}" """.format((root.dirOrFile.replace("&", "...")))
 
     if root.children:
         # if the root is a directory
         WholeText = WholeText + newText
         WholeText += ">\n"
-        for i in root.children:
-            WholeText, WholeTextEnd = print_children(WholeText, WholeTextEnd, i)
+        for subroot in root.children:
+            WholeText, WholeTextEnd = print_children(WholeText, WholeTextEnd, subroot, Path(path, subroot.dirOrFile))
         WholeText += "</node>\n"
     else:
         # if the root is a file
+
         WholeText = WholeText + newText
-        WholeText += "/>\n"
+        WholeText += ' LINK="{link}"/>\n'.format(link=path)
 
     return WholeText, WholeTextEnd
 
@@ -102,10 +76,10 @@ if __name__ == "__main__":
     # r=root, d=directories, f = files
     for r, d, f in os.walk(filename1):
         for file in f:
-            fsss.append(os.path.join(r, file))
-    root = TrieNode('*')
+            fsss.append(Path(r, file))
+    root = TrieNode("")
     WholeText = """<map version = "1.0.1" >
-                    <node CREATED="1548918823219" ID="ID_1805243291" MODIFIED="1548918823219" TEXT="New Mindmap">"""
+                    <node CREATED="1548918823219" ID="ID_1805243291" MODIFIED="1548918823219" TEXT="Directory Map">"""
     WholeTextEnd = """</node>
         </map>"""
 
@@ -117,7 +91,7 @@ if __name__ == "__main__":
     countec = root.counter
 
     if countec > 0:
-        WholeText, WholeTextEnd=print_children(WholeText, WholeTextEnd, root)
+        WholeText, WholeTextEnd=print_children(WholeText, WholeTextEnd, root, root.dirOrFile)
     print(WholeText, WholeTextEnd)
     with open(r"C:\DavidB\MindMaps\first.mm", 'w') as fh:
         fh.write(WholeText)
